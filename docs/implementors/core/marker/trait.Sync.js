2f445418d3c562d58d140e507de590aa389697d8
Hitomaru Horino <type10tk@kahi-sv.info>
2019-12-23 00:50:10 +0900

    add docs

---
+++ docs/implementors/core/marker/trait.Sync.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["naromat"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"struct\" href=\"naromat/entities/chapter/struct.Chapter.html\" title=\"struct naromat::entities::chapter::Chapter\">Chapter</a>",synthetic:true,types:["naromat::entities::chapter::Chapter"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"struct\" href=\"naromat/entities/line/struct.Line.html\" title=\"struct naromat::entities::line::Line\">Line</a>",synthetic:true,types:["naromat::entities::line::Line"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"struct\" href=\"naromat/entities/sentence/struct.Sentence.html\" title=\"struct naromat::entities::sentence::Sentence\">Sentence</a>",synthetic:true,types:["naromat::entities::sentence::Sentence"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
