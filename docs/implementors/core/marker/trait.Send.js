e5caef27e86658b45d6aaa49a6d9bb5797d9b178
Hitomaru Horino <40270037+Hitomaru@users.noreply.github.com>
2019-12-23 00:52:01 +0900

    Feature/add infos into cargo.toml (#20)
    * Add license
    
    * add docs
    
    * Set next version

---
+++ docs/implementors/core/marker/trait.Send.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["naromat"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"naromat/entities/chapter/struct.Chapter.html\" title=\"struct naromat::entities::chapter::Chapter\">Chapter</a>",synthetic:true,types:["naromat::entities::chapter::Chapter"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"naromat/entities/line/struct.Line.html\" title=\"struct naromat::entities::line::Line\">Line</a>",synthetic:true,types:["naromat::entities::line::Line"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"naromat/entities/sentence/struct.Sentence.html\" title=\"struct naromat::entities::sentence::Sentence\">Sentence</a>",synthetic:true,types:["naromat::entities::sentence::Sentence"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ docs/naromat/entities/line/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Line","Structure of novel line"]]});
