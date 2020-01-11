ae06d7a2faf82b545be276a7fd0d71c6fe2e178b
= <=>
2020-01-11 15:29:17 +0900

    Document  methods

--- src/entities/file.rs
+++ src/entities/file.rs
@@ -4,15 +4,23 @@ use std::fs::File;
 use std::io::{ Write };
 
 /// Structure of Novel text file
-/// 
-/// 
 pub struct TextFile {
     path_to : String,
     chapter : Chapter,
 }
 
+/// Implementation for novel text structure
 impl TextFile {
 
+    /// Constructor
+    /// 
+    /// # Example
+    /// 
+    /// ```no_run
+    /// use naromat::entities::file::TextFile;
+    /// 
+    /// TextFile::new("./path/to/source/file","./path/to/save");
+    /// ```
     pub fn new(path_from : &str, path_to : &str) -> Result<Self, Box<dyn std::error::Error + 'static>> {
         let text = fs::read_to_string(path_from)?;
         Ok(Self {
@@ -21,6 +29,20 @@ impl TextFile {
         })
     }
 
+    /// Format file text and save
+    /// 
+    /// Return true if save was succeed
+    /// 
+    /// Return false if save was failed
+    /// 
+    /// # Example
+    /// 
+    /// ```no_run
+    /// use naromat::entities::file::TextFile;
+    /// 
+    /// let text = TextFile::new("./path/to/source/file","./path/to/save").unwrap();
+    /// text.format_and_save();
+    /// ```
     pub fn format_and_save(self) -> bool {
         let mut file = match File::create(self.path_to) {
             Ok(f) => f,
@@ -57,6 +79,8 @@ mod tests {
         
         // teardown
         fs::remove_file(target_file_path).unwrap();
+        
+        // assert
         assert!(is_target_text_is_same_to_reference);
     }
 }
