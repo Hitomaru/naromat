74d61fa5be539b9ed0f52c5e7ff1152fc19c2c4c
Hitomaru Horino <40270037+Hitomaru@users.noreply.github.com>
2020-01-12 02:52:16 +0900

    Feature/rustfmt (#23)
    * Setup rustfmt & clippy
    * Fix source as linted by tools 
    * Parallelize actions
    * Stabilize some tests

--- src/entities/sentence.rs
+++ src/entities/sentence.rs
@@ -1,8 +1,8 @@
-use regex::Regex;
 use regex::Captures;
+use regex::Regex;
 
 /// Structure of novel sentence.
-/// 
+///
 /// Sentences are defined below:
 /// * Starts from previous sentence or breakline
 /// * End with sentence-terminators ('.', '。', '」'))
@@ -12,33 +12,35 @@ use regex::Captures;
 ///     * '！'
 ///     * '？'
 ///     * '!?'
-/// 
+///
 pub struct Sentence {
-    elements : String,
+    elements: String,
 }
 
 /// Implementation for novel sentence structure
 impl Sentence {
     /// Constructor
-    /// 
+    ///
     /// # Example
-    /// 
+    ///
     /// ```
     /// use naromat::entities::sentence::Sentence;
-    /// 
+    ///
     /// Sentence::new("我が輩は猫である。");
     /// ```
-    pub fn new(sentence : &str) -> Self {
-        Self { elements: sentence.trim_end().to_string() }
+    pub fn new(sentence: &str) -> Self {
+        Self {
+            elements: sentence.trim_end().to_string(),
+        }
     }
 
     /// Print formatted sentence
-    /// 
+    ///
     /// # Example
-    /// 
+    ///
     /// ```
     /// use naromat::entities::sentence::Sentence;
-    /// 
+    ///
     /// let sentence = Sentence::new("我が輩は猫である。");
     /// sentence.print()
     /// ```
@@ -47,17 +49,17 @@ impl Sentence {
     }
 
     /// Get string of formatted sentence
-    /// 
+    ///
     /// # Example
-    /// 
+    ///
     /// ```
     /// use naromat::entities::sentence::Sentence;
     /// let sentence = Sentence::new("我が[輩:.]は[猫:ねこ]である");
     /// assert_eq!(sentence.get(), "我が｜輩《・》は｜猫《ねこ》である");
     /// ```
-    /// 
+    ///
     pub fn get(self) -> String {
-        self.format().elements.to_string()
+        self.format().elements
     }
 
     /// Format sentence
@@ -77,13 +79,11 @@ impl Sentence {
     /// Convert kenten(圏点) format to Narou format.
     fn convert_kenten(&self) -> Self {
         let kenten = Regex::new(r"\[([^]]*?):\.\]").unwrap();
-        let sentence = kenten.replace_all(&self.elements, |caps: &Captures| {
-            format!(
-                "｜{}《{}》",
-                &caps[1],
-                "・".to_string().repeat(caps[1].chars().count()
-            ))
-        }).to_string();
+        let sentence = kenten
+            .replace_all(&self.elements, |caps: &Captures| {
+                format!("｜{}《{}》", &caps[1], "・".to_string().repeat(caps[1].chars().count()))
+            })
+            .to_string();
         Self::new(&sentence)
     }
 
@@ -101,14 +101,14 @@ mod tests {
 
     #[test]
     fn get() {
-        let source   = "私の[名前:なまえ]は！[田中:.]？太郎!?です";
+        let source = "私の[名前:なまえ]は！[田中:.]？太郎!?です";
         let expected = "私の｜名前《なまえ》は！　｜田中《・・》？　太郎!?　です";
         let sentence = Sentence::new(&source);
         assert_eq!(sentence.get(), expected);
     }
     #[test]
     fn format() {
-        let source   = "私の[名前:なまえ]は！[田中:.]？太郎!?です";
+        let source = "私の[名前:なまえ]は！[田中:.]？太郎!?です";
         let expected = "私の｜名前《なまえ》は！　｜田中《・・》？　太郎!?　です";
         let sentence = Sentence::new(&source).format();
         assert_eq!(sentence.elements, expected);
@@ -116,7 +116,7 @@ mod tests {
 
     #[test]
     fn format_min() {
-        let source   = "私";
+        let source = "私";
         let expected = "私";
         let sentence = Sentence::new(&source).format();
         assert_eq!(sentence.elements, expected);
@@ -134,14 +134,13 @@ mod tests {
     fn convert_ruby_should_convert_ruby() {
         let sut = Sentence::new("私の[名前:なまえ]は[太郎:たろう]です");
         let expected = "私の｜名前《なまえ》は｜太郎《たろう》です";
-        assert_eq!(sut.convert_ruby().elements, expected);  
+        assert_eq!(sut.convert_ruby().elements, expected);
     }
 
     #[test]
     fn convert_kenten_should_convert_kenten() {
         let sut = Sentence::new("私の[名前:.]は[たろう:.]です");
         let expected = "私の｜名前《・・》は｜たろう《・・・》です";
-        assert_eq!(sut.convert_kenten().elements, expected);  
+        assert_eq!(sut.convert_kenten().elements, expected);
     }
-
 }
--- src/entities/text_file.rs
+++ src/entities/text_file.rs
@@ -2,73 +2,73 @@ use crate::entities::chapter::Chapter;
 use crate::errors::TextFileOutputError;
 use std::fs;
 use std::fs::File;
-use std::io::{ Write };
+use std::io::Write;
 use std::path::Path;
 
 /// Structure of Novel text file
 pub struct TextFile {
-    chapter : Chapter,
+    chapter: Chapter,
 }
 
 /// Implementation for novel text structure
 impl<'file_handling> TextFile {
-
     /// Constructor
     ///
     /// # Fail
-    /// 
+    ///
     /// * Path format is invalid
     /// * A file designated by a path is not exists
     ///  
     /// # Example
-    /// 
+    ///
     /// ```no_run
     /// use naromat::entities::text_file::TextFile;
-    /// 
+    ///
     /// TextFile::new("./path/to/source/file.txt").unwrap();
     /// ```
-    pub fn new(path_from : &str) -> Result<Self, Box<dyn std::error::Error + 'static>> {
+    pub fn new(path_from: &str) -> Result<Self, Box<dyn std::error::Error + 'static>> {
         let text = fs::read_to_string(path_from)?;
-        Ok(Self { chapter: Chapter::new(text.as_str()) })
+        Ok(Self {
+            chapter: Chapter::new(text.as_str()),
+        })
     }
 
     /// Format file text and save
-    /// 
+    ///
     /// Return true if save was succeed
-    /// 
+    ///
     /// Return false if save was failed
-    /// 
+    ///
     /// # Example
-    /// 
+    ///
     /// ```no_run
     /// use naromat::entities::text_file::TextFile;
-    /// 
+    ///
     /// let text = TextFile::new("./path/to/source/file.txt").unwrap();
     /// text.format_and_save("./path/to/save.txt");
     /// ```
-    pub fn format_and_save(self, path_to : &'file_handling str) -> Result<(),TextFileOutputError> {
-        Self::touch_file(path_to)
-            .and_then(|file| self.save_file(file))
+    pub fn format_and_save(self, path_to: &'file_handling str) -> Result<(), TextFileOutputError> {
+        Self::touch_file(path_to).and_then(|file| self.save_file(file))
     }
 
-    fn touch_file(path_to : &'file_handling str) -> Result<File, TextFileOutputError> {
+    fn touch_file(path_to: &'file_handling str) -> Result<File, TextFileOutputError> {
         if Path::new(path_to).exists() {
             return Err(TextFileOutputError::AlreadyExists(path_to));
         }
         match File::create(path_to) {
-            Ok(file)   => Ok(file),
-            Err(cause)  => Err(TextFileOutputError::CannotCreate(cause)),
+            Ok(file) => Ok(file),
+            Err(cause) => Err(TextFileOutputError::CannotCreate(cause)),
         }
     }
 
-    fn save_file(self, mut file : File) -> Result<(), TextFileOutputError<'file_handling>> {
-        let result = match write!(file, "{}\n", self.chapter.get()) {
+    fn save_file(self, mut file: File) -> Result<(), TextFileOutputError<'file_handling>> {
+        let result = match writeln!(file, "{}", self.chapter.get()) {
             Ok(_) => Ok(()),
             Err(cause) => return Err(TextFileOutputError::CannotWrite(cause)),
         };
         match file.flush() {
             Ok(_) => result,
-            Err(cause) => return Err(TextFileOutputError::CannotFlush(cause))
+            Err(cause) => Err(TextFileOutputError::CannotFlush(cause)),
         }
     }
 }
@@ -76,27 +76,28 @@ impl<'file_handling> TextFile {
 #[cfg(test)]
 mod tests {
     use super::TextFile;
-    use file_diff::{diff};
-    use std::fs;
+    use file_diff::diff;
+    use std::{fs, time};
 
     #[test]
     fn can_save_formatted_text() {
         // given
         let source_file_path = "./resources/test/entities/file/source.txt";
         let reference_file_path = "./resources/test/entities/file/reference.txt";
-        let target_file_path = "./resources/test/entities/file/target.txt";
-        let target_file = TextFile::new(source_file_path).unwrap();
+        let now = time::Instant::now();
+        let target_file_path = format!(
+            "./resources/test/entities/file/target-{}.txt",
+            now.elapsed().as_millis()
+        );
+        let source_file = TextFile::new(source_file_path).unwrap();
 
-        
         // when
-        target_file.format_and_save(target_file_path).unwrap();
+        source_file.format_and_save(target_file_path.as_str()).unwrap();
 
         // then
-        let is_target_text_is_same_to_reference = diff(target_file_path, reference_file_path);
-        
+        let is_target_text_is_same_to_reference = diff(target_file_path.as_str(), reference_file_path);
         // teardown
         fs::remove_file(target_file_path).unwrap();
-        
         // assert
         assert!(is_target_text_is_same_to_reference);
     }
@@ -105,11 +106,11 @@ mod tests {
     fn return_error_when_target_file_already_exists() {
         // given
         let source_file_path = "./resources/test/entities/file/source.txt";
-        let target_file_path = "./resources/test/entities/file/target.txt";
-        let target_file = TextFile::new(source_file_path).unwrap();
+        let target_file_path = "./resources/test/entities/file/already_exists.txt";
+        let source_file = TextFile::new(source_file_path).unwrap();
 
         // expect
-        let is_return_error = target_file.format_and_save(target_file_path).is_err();
+        let is_return_error = source_file.format_and_save(target_file_path).is_err();
         let is_target_file_not_modified = fs::metadata(target_file_path).unwrap().len() == 0;
 
         // assert
