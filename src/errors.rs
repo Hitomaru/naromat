79630d22740e19cd52fd195fe1226155f1292626
Hitomaru Horino <type10tk@kahi-sv.info>
2020-01-13 15:36:37 +0900

    introduce recursive convert

--- src/errors.rs
+++ src/errors.rs
@@ -1,5 +1,6 @@
 use std::fmt;
 use std::io;
+use std::path::Path;
 
 #[derive(Debug)]
 pub enum TextFileOutputError<'file_handling> {
@@ -18,6 +19,46 @@ impl<'file_handling> fmt::Display for TextFileOutputError<'file_handling> {
             TextFileOutputError::AlreadyExists(path) => {
                 write!(f, "Already exists error: File {} is already exists", path)
             }
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum FileProcessError<'file_process> {
+    InvalidPath(InvalidPathError<'file_process>),
+    SaveError(TextFileOutputError<'file_process>),
+    LoadError(Box<dyn std::error::Error>),
+}
+
+impl<'file_process> fmt::Display for FileProcessError<'file_process> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "File process failed")
+    }
+}
+
+#[derive(Debug)]
+pub enum DirectoryProcessError<'file_process> {
+    InvalidPath(InvalidPathError<'file_process>),
+    SaveError(TextFileOutputError<'file_process>),
+    LoadError(Box<dyn std::error::Error>),
+    Other,
+}
+
+impl<'file_process> fmt::Display for DirectoryProcessError<'file_process> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "File process failed")
+    }
+}
+
+#[derive(Debug)]
+pub enum InvalidPathError<'file_handling> {
+    FileNotFound(&'file_handling Path),
+}
+
+impl<'file_handling> fmt::Display for InvalidPathError<'file_handling> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            InvalidPathError::FileNotFound(path) => write!(f, "Not exists error: File {} is not found", path.display()),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate clap;
 
 use clap::{App, Arg};
 use naromat::entities::text_file::TextFile;
+use naromat::errors::*;
 use std::path::Path;
 
 fn main() {
@@ -19,48 +20,64 @@ fn main() {
         );
     let matches = app.get_matches();
     let source = matches.value_of("source").unwrap_or("./");
-    let source_path = Path::new(source);
+    let source = Path::new(source);
     let target = matches.value_of("target").unwrap_or("./");
-    if source_path.is_file() {
-        process_file(source, target);
+    if source.is_file() {
+        match process_file(source, target) {
+            Ok(_) => 0,
+            Err(_) => 1,
+        };
     } else {
-        process_dir(source_path).unwrap();
+        match process_dir(source, target) {
+            Ok(_) => 0,
+            Err(_) => 1,
+        };
     }
 }
 
-fn process_file(source: &str, target: &str) {
-    match TextFile::new(source) {
+fn process_file<'file_process>(
+    source: &Path,
+    target: &'file_process str,
+) -> Result<(), FileProcessError<'file_process>> {
+    match TextFile::new(stringify_path(source).unwrap()) {
         Ok(file) => {
-            println!("file {} loaded", source);
+            println!("file {} loaded", source.display());
             match file.format_and_save(target) {
                 Ok(_) => {
-                    println!("file '{}' successfuly saved", target);
+                    println!("the file '{}' successfuly saved", target);
+                    Ok(())
                 }
                 Err(err) => {
-                    println!("file '{}' cannot be saved: {}", target, err);
+                    println!("the file '{}' cannot be saved: {}", target, err);
+                    Err(FileProcessError::SaveError(err))
                 }
-            };
+            }
         }
         Err(err) => {
-            println!("the file '{}' cannot be loaded: {}", source, err);
+            println!("the file '{}' cannot be loaded: {}", source.display(), err);
+            Err(FileProcessError::LoadError(err))
         }
-    };
+    }
 }
-fn process_dir(source: &Path) -> Result<(), Box<dyn std::error::Error + 'static>> {
+
+fn stringify_path<'file_process>(path: &Path) -> Result<&str, InvalidPathError> {
+    match path.to_str() {
+        Some(string) => Ok(string),
+        None => Err(InvalidPathError::FileNotFound(path)),
+    }
+}
+fn process_dir(source: &Path, target: &str) -> Result<(), Box<dyn std::error::Error + 'static>> {
     for entry in source.read_dir()? {
         let entry = entry?;
         let path = entry.path();
         if path.is_dir() {
-            process_dir(&path)?
+            process_dir(&path, target)?
         } else {
             let parent_dir = path.parent().unwrap().to_str().unwrap();
-            let file_name = path.file_name().unwrap().to_str().unwrap();
-            let file_name = format!(
-                "{}/formatted_{}",
-                parent_dir,
-                file_name
-            );
-            TextFile::new(path.to_str().unwrap())?.format_and_save(file_name.as_str()).unwrap();
+            let file_name = path.file_name().unwrap().to_str().expect("File name cannot be parsed");
+            let file_path = stringify_path(&path).expect("File path cannot be parsed");
+            let file_name = format!("{}/formatted_{}", parent_dir, file_name);
+            TextFile::new(file_path)?.format_and_save(file_name.as_str()).unwrap()
         }
     }
     Ok(())
