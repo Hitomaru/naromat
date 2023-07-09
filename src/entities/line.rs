407daf322d838cf4dba9797c8d9f0fa45715cc22
Hitomaru Horino <type10tk@kahi-sv.info>
2023-07-09 16:01:28 +0000

    rustfmt

--- src/entities/line.rs
+++ src/entities/line.rs
@@ -58,10 +58,7 @@ impl Line {
     /// Format line
     fn format(text: &str) -> Self {
         let line = Self::add_header_space(text.trim());
-        let line = Self::split(&line)
-            .into_iter()
-            .map(Sentence::new)
-            .collect();
+        let line = Self::split(&line).into_iter().map(Sentence::new).collect();
         Self { elements: line }
     }
 
